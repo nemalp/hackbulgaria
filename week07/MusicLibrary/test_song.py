8e8aa4e2208a4ee084d59e9d77e59019ba794b0f
nemalp <lazarov.pl@gmail.com>
2016-11-29 23:58:23 +0200

    Add tests for song

--- week07/MusicLibrary/test_song.py
+++ week07/MusicLibrary/test_song.py
@@ -11,6 +11,9 @@ class TestSong(unittest.TestCase):
         self.s1 = Song(title="Odin", artist="Manowar",
                        album="The Sons of Odin", length="3:44")
 
+        self.s2 = Song(title="Die for Metal", artist="Manowar",
+                       album="New", length="1:3:44")
+
     def test_init(self):
         self.assertTrue(isinstance(self.s, Song))
 
@@ -21,10 +24,17 @@ class TestSong(unittest.TestCase):
     def test_eq(self):
         self.assertEqual(self.s, self.s1)
 
+    def test_get_seconds(self):
+        self.assertEqual(self.s.get_seconds('2:20'), 140)
+        self.assertEqual(self.s.get_seconds('1:2:20'), 3740)
+
     def test_length(self):
-        pass
-       #  self.assertEqual(self.s.length(seconds=True),
-       #                  self.s.parse_length('3:44'))
+        self.assertEqual(self.s.length(seconds=True), 224)
+        self.assertEqual(self.s2.length(seconds=True), 3824)
+        self.assertEqual(self.s.length(minutes=True), 3)
+        self.assertEqual(self.s2.length(hours=True), 1)
+        with self.assertRaises(ValueError):
+            self.s.length(hours=True)
 
 
 if __name__ == '__main__':
