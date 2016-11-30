bd661605bb06790963a6e5fc767eb3c75e5665bb
Lazarov <plamen.lazarov@accenture.com>
2016-11-30 14:48:41 +0200

    Add test for artist

--- week07/MusicLibrary/test_playlist.py
+++ week07/MusicLibrary/test_playlist.py
@@ -13,11 +13,14 @@ class TestPlaylist(unittest.TestCase):
         self.s = Song(title="Odin", artist="Manowar", album="The Sons of Odin",
                       length="3:10")
 
-        self.s1 = Song(title="Die for Metal", artist="Manowar",
+        self.s1 = Song(title="Odin", artist="Manowar",
                        album="Unknown", length="5:10")
 
         self.s2 = Song(title="The crown and the ring", artist="Manowar",
                        album="New", length="7:10")
+
+        self.s3 = Song(title="Cowboys from hell", artist="Pantera",
+                       album="Cowboys from hell", length="5:10")
 
     def test_init(self):
         self.assertTrue(isinstance(self.pl, Playlist))
@@ -47,7 +50,8 @@ class TestPlaylist(unittest.TestCase):
         self.assertEqual(self.pl.total_length(), '0:15:30')
 
     def test_artists(self):
-        pass
+        self.pl.add_songs([self.s, self.s1, self.s2, self.s3])
+        self.assertEqual({'Manowar': 3, 'Pantera': 1}, self.pl.artists())
 
     def test_next_song(self):
         pass
