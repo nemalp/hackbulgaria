adfcc3648e4804c97e6fb17e57bee4a4f7ba6932
nemalp <lazarov.pl@gmail.com>
2017-01-09 22:30:57 +0200

    Refactor sql_manager

--- week11/Money-In-The-Bank/sql_manager.py
+++ week11/Money-In-The-Bank/sql_manager.py
@@ -1,44 +1,38 @@
 import sqlite3
 from client import Client
+from settings.general_settings import (DB_NAME)
+from queries.queries import (CREATE_CLIENT_TABLE, UPDATE_CLIENT_MESSAGE,
+                             UPDATE_CLIENT_PASSWORD, REGISTER_CLIENT,
+                             SELECT_CLIENT)
 
-conn = sqlite3.connect("bank.db")
+
+conn = sqlite3.connect(DB_NAME)
 cursor = conn.cursor()
 
 
 def create_clients_table():
-    create_query = '''create table if not exists
-        clients(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                username TEXT,
-                password TEXT,
-                balance REAL DEFAULT 0,
-                message TEXT)'''
-
-    cursor.execute(create_query)
+    cursor.execute(CREATE_CLIENT_TABLE)
+    conn.commit()
 
 
 def change_message(new_message, logged_user):
-    update_sql = "UPDATE clients SET message = '%s' WHERE id = '%s'" % (new_message, logged_user.get_id())
-    cursor.execute(update_sql)
+    cursor.execute(UPDATE_CLIENT_MESSAGE, [new_message, logged_user.get_id()])
     conn.commit()
     logged_user.set_message(new_message)
 
 
 def change_pass(new_pass, logged_user):
-    update_sql = "UPDATE clients SET password = '%s' WHERE id = '%s'" % (new_pass, logged_user.get_id())
-    cursor.execute(update_sql)
+    cursor.execute(UPDATE_CLIENT_PASSWORD, [new_pass, logged_user.get_id()])
     conn.commit()
 
 
 def register(username, password):
-    insert_sql = "insert into clients (username, password) values ('%s', '%s')" % (username, password)
-    cursor.execute(insert_sql)
+    cursor.execute(REGISTER_CLIENT, [username, password])
     conn.commit()
 
 
 def login(username, password):
-    select_query = "SELECT id, username, balance, message FROM clients WHERE username = '%s' AND password = '%s' LIMIT 1" % (username, password)
-
-    cursor.execute(select_query)
+    cursor.execute(SELECT_CLIENT, [username, password])
     user = cursor.fetchone()
 
     if(user):
