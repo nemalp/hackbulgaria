4cba940ac3eadb849c4eea2e2171f53e5bddf98c
nemalp <lazarov.pl@gmail.com>
2017-01-19 22:27:40 +0200

    Improve book reader

--- week12/Generators/Book-Reader/book_reader.py
+++ week12/Generators/Book-Reader/book_reader.py
@@ -1,32 +1,30 @@
 import zipfile
-import getch
+from getch import getch
 
 
 def book_reader():
     with zipfile.ZipFile('Book.zip') as z:
         zip_content = [f.filename for f in z.infolist()]
 
-    for file_ in zip_content:
-        with open(file_, 'r') as f:
-            line = f.readline()
+        for file_ in zip_content:
+            with z.open(file_, 'r') as f:
+                line = f.readline().decode('utf-8')
 
-            while line:
-                result = ''
-                if str(line).startswith('#'):
-                    line = f.readline()
+                while line:
+                    result = ''
+                    result += line
+                    line = f.readline().decode('utf-8')
 
-                    while str(line).startswith('#') is False:
+                    while str(line).startswith('#') is False and \
+                            str(line) != '':
                         result += line
-                        line = f.readline()
+                        line = f.readline().decode('utf-8')
 
                     yield result
-
-                line = f.readline()  # chapter
 
 book = book_reader()
 
 for chapter in book:
-    space = getch.getch() == ' '
-    # enter = getch.getch() == '\n'
-    if space:
+    space = getch()
+    if space == ' ':
         print(chapter)
