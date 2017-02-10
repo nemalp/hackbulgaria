9a41b0fa84f1bcd9d6dddd42d8620598848ac79f
nemalp <lazarov.pl@gmail.com>
2017-02-10 16:04:22 +0000

    Add course urls

--- week15/Course-Management/course_management/courses/urls.py
+++ week15/Course-Management/course_management/courses/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from courses import views
 
 urlpatterns = [
-    url(r'(?P<course>[a-zA-z]+)/$', views.course_details, name='course_detals'),
     url(r'^new/$', views.create_course, name='create_course'),
+    url(r'(?P<course>[a-zA-z]+)/$', views.course_details, name='course_detals'),
 ]
