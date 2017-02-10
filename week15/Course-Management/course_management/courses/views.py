fb19676360813aa253f79eb4e703cef85fd0d121
nemalp <lazarov.pl@gmail.com>
2017-02-10 16:04:57 +0000

    Add create_course and course_details view

--- week15/Course-Management/course_management/courses/views.py
+++ week15/Course-Management/course_management/courses/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from courses.models import Course
+import ipdb
 
 
 def course_details(request, *args, **kwargs):
+    courses = Course.objects.all()
     return render(request, 'course_details.html', locals())
 
 
-def create_course(request):
-    pass
+def create_course(request, *args, **kwargs):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        # ipdb.set_trace()
+        if Course.objects.filter(name=name).exists():
+            course = True
+            return render(request, 'create_course.html', locals())
+
+        description = request.POST.get('description')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+
+        Course.objects.create(name=name, description=description, 
+                              start_date=start_date, end_date=end_date)
+
+        return redirect('/')
+
+    return render(request, 'create_course.html', locals())
