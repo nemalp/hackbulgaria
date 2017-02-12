9ea8117c8fe413c8d58857b7d82b2d018149563b
nemalp <lazarov.pl@gmail.com>
2017-02-12 08:56:12 +0200

    Add courses models

--- week15/Course-Management/course_management/courses/models.py
+++ week15/Course-Management/course_management/courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=60, unique=True)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
