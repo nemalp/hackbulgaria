993accf574b39db3d33a9358a3ee7f5a88427281
nemalp <lazarov.pl@gmail.com>
2017-02-12 08:54:19 +0200

    Add lectures models

--- week15/Course-Management/course_management/lectures/models.py
+++ week15/Course-Management/course_management/lectures/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from courses.models import Course
 
-# Create your models here.
+
+class Lecture(models.Model):
+    name = models.CharField(max_length=60)
+    week = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    url = models.CharField(max_length=1000)
