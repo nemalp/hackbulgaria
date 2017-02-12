fe2ca14508907a5c39c2584ee1efbf8d6e1e03e6
nemalp <lazarov.pl@gmail.com>
2017-02-12 08:57:12 +0200

    Add courses urls

--- week15/Course-Management/course_management/courses/urls.py
+++ week15/Course-Management/course_management/courses/urls.py
@@ -3,5 +3,9 @@ from courses import views
 
 urlpatterns = [
     url(r'^new/$', views.create_course, name='create_course'),
-    url(r'(?P<course>[a-zA-z]+)/$', views.course_details, name='course_detals'),
+    url(r'^edit/(?P<course>[a-zA-Z\d\s]+)/$',
+        views.edit_course, name='edit_course'),
+    url(r'(?P<course_name>[a-zA-Z\d\s]+)/$',
+        views.course_details, name='course_details')
+
 ]
