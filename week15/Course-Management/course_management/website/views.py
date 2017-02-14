9e558d74a483d28962b6f176db787f1b093267dc
nemalp <lazarov.pl@gmail.com>
2017-02-15 00:21:15 +0200

    Add views

--- week15/Course-Management/course_management/website/views.py
+++ week15/Course-Management/course_management/website/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from base_app.models import User
 import ipdb
 
 
 def register(request):
+    # TODO implement decorator for checking if user is logged
+    if request.session.get('email'):
+        return redirect('/profile/')
+
     if request.method == 'POST':
         email = request.POST['email']
         passw = request.POST['password']
@@ -17,4 +22,26 @@ def register(request):
 
 
 def login(request):
-    return render(request, 'auth/register.html', locals())
+    # TODO implement decorator for checking if user is logged
+    if request.session.get('email'):
+        return redirect('/profile/')
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        if User.login(email, password):
+            request.session['email'] = email
+            return redirect('/profile/')
+        else:
+            error_msg = 'Invalid username or password'
+
+    return render(request, 'auth/login.html', locals())
+
+
+def profile(request):
+    # TODO implement decorator for checking if user is logged
+    if not request.session.get('email'):
+        return redirect('/login/')
+
+    return render(request, 'partials/profile.html')
